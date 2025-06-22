385649748a977753adbfcb40103b9eb420a3ca8d
0sureshyadav0 <suresh.y.np@gmail.com>
2025-06-23 02:48:55 +0545

    our services have been done

---
+++ carousel.js
@@ -0,0 +1,15 @@
+// const carousel = document.querySelector(".cards");
+// let scrollAmount = 0;
+
+// function autoScroll() {
+//   scrollAmount += 1;
+//   if (scrollAmount >= carousel.scrollWidth - carousel.clientWidth) {
+//     scrollAmount = 0;
+//   }
+//   carousel.scrollTo({
+//     left: scrollAmount,
+//     behavior: "smooth",
+//   });
+// }
+
+// setInterval(autoScroll, 12); // Adjust speed here
